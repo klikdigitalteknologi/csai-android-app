644806f5056a81deffa4a69f08b4b7e49db1927c
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-12-24 23:08:26 +0800

    [+] fix home

--- app/src/main/java/id/klikdigital/csaiapp/home/Home.java
+++ app/src/main/java/id/klikdigital/csaiapp/home/Home.java
@@ -3,13 +3,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,27 +25,42 @@ import com.google.android.material.navigation.NavigationView;
 import id.klikdigital.csaiapp.R;
 import id.klikdigital.csaiapp.login.activity.LoginActivity;
 import id.klikdigital.csaiapp.session.SessionManage;
-public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, FragmentManager.OnBackStackChangedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ImageView imageView,btn_close;
+    private CardView layoutoolbar;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
+        layoutoolbar = findViewById(R.id.layoutToolBarHome);
         drawerLayout = findViewById(R.id.drawer_layout_home);
         navigationView = findViewById(R.id.nav_view);
         navigationView.bringToFront();
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
         imageView = findViewById(R.id.image_home);
+
         btn_close =navigationView.getHeaderView(0).findViewById(R.id.btnClose);
         imageView.setOnClickListener(view -> drawerLayout.openDrawer(GravityCompat.START));
         btn_close.setOnClickListener(view -> drawerLayout.closeDrawer(GravityCompat.START));
+
+        // Temukan NavHostFragment dan dapatkan NavController
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavController navController = navHostFragment.getNavController();
+
+        // Tambahkan Listener untuk OnBackStackChanged
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> navigationView.setCheckedItem(destination.getId()));
+    }
+    @Override
+    public void onBackStackChanged() {
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        int currentDestinationId = navController.getCurrentDestination().getId();
+        navigationView.setCheckedItem(currentDestinationId);
     }
 
     @Override
@@ -68,9 +89,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         return false;
     }
     private void navigateToFragment(int fragmentId) {
-        drawerLayout.closeDrawer(GravityCompat.START);
         NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
         navController.navigate(fragmentId);
+        drawerLayout.closeDrawer(GravityCompat.START);
     }
     private void showLogoutConfirmationDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -107,5 +128,17 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         });
         popupMenu.show();
     }
+
+    public void hideToolbar() {
+        if (layoutoolbar != null) {
+            layoutoolbar.setVisibility(View.GONE);
+        }
+    }
+    public void showToolbar(){
+        if (layoutoolbar != null){
+            layoutoolbar.setVisibility(View.VISIBLE);
+        }
+    }
+
 
 }
