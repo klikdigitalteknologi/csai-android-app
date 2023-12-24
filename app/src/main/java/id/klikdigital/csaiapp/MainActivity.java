89a87a6be04b90002874e6d726896a4d30f58c1e
nandamodjo21 <jefrandamodjo.mhs20@ti.poligon.ac.id>
2023-12-24 23:09:29 +0800

    [+] fix MainActivity

--- app/src/main/java/id/klikdigital/csaiapp/MainActivity.java
+++ app/src/main/java/id/klikdigital/csaiapp/MainActivity.java
@@ -17,6 +17,8 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -28,6 +30,7 @@ import id.klikdigital.csaiapp.chat.interfaces.ChatService;
 import id.klikdigital.csaiapp.chat.response.SendImageResponse;
 import id.klikdigital.csaiapp.config.Config;
 import id.klikdigital.csaiapp.config.ConfigPrivate;
+import id.klikdigital.csaiapp.fragment.ChatFragment;
 import id.klikdigital.csaiapp.fragment.LogoutPopup;
 import id.klikdigital.csaiapp.login.activity.LoginActivity;
 import id.klikdigital.csaiapp.session.SessionManage;
@@ -40,7 +43,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity{
 
-    private ImageView imageSender;
+    private ImageView imageSender,bck;
     private String gambar,whatsapp,sender,message;
     private EditText chat;
     private AppCompatImageView btnSend;
@@ -51,66 +54,78 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageSender = findViewById(R.id.terimagambar);
+        bck = findViewById(R.id.backtoChatroom);
         chat = findViewById(R.id.chatEditext);
         btnSend = findViewById(R.id.btnSendImage);
         SessionManage sessionManage = new SessionManage(this);
         sender = sessionManage.getUserData().getMemberKode();
-        gambar = getIntent().getStringExtra("imagePath");
-        whatsapp = getIntent().getStringExtra("whatsapp");
-       Log.d("RESPONSE",gambar);
-       if (gambar != null){
-           Picasso.get().load("file://"+gambar).into(imageSender);
-           imageSender.setVisibility(View.VISIBLE);
-       }else {
-           Toast.makeText(getApplicationContext(),"GAMBAR KOSONG",Toast.LENGTH_SHORT).show();
-       }
-       btnSend.setOnClickListener(view -> {
-           message = chat.getText().toString();
-           sendIamge();
-       });
-    }
-    private void sendIamge() {
-        String aa = "/" + gambar;
-        RequestBody senderBody = RequestBody.create(MediaType.parse("text/plain"), sender);
-        RequestBody waBody = RequestBody.create(MediaType.parse("text/plain"), whatsapp);
-        RequestBody pesanBody = RequestBody.create(MediaType.parse("text/plain"), message);
-
-        RequestBody imageBody = RequestBody.create(MediaType.parse("image/*"), gambar);
-        MultipartBody.Part imagePart = MultipartBody.Part.createFormData("file",gambar, imageBody);
-        ChatService chatService = ConfigPrivate.htppclient().create(ChatService.class);
-        Call<SendImageResponse>call = chatService.sendImage(senderBody,waBody,imagePart,pesanBody);
-        call.enqueue(new Callback<SendImageResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<SendImageResponse> call, @NonNull Response<SendImageResponse> response) {
-                if (response.isSuccessful()){
-                    SendImageResponse sendImageResponse = response.body();
-                    if (sendImageResponse.isStatus()){
-                        Toast.makeText(getApplicationContext(),"sedang mengirim",Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(),ChatRoom.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-                        intent.putExtra("nomor_wa",whatsapp);
-                        startActivity(intent);
-                    }else {
-                        Toast.makeText(getApplicationContext(),"Gagal mengirim gambar",Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(getApplicationContext(),"server not responding",Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<SendImageResponse> call, @NonNull Throwable t) {
-                Log.d("Response Send Image",t.getMessage());
-            }
+        bck.setOnClickListener(view -> {
+            backtoChatlist();
         });
+//        gambar = getIntent().getStringExtra("imagePath");
+////        whatsapp = getIntent().getStringExtra("whatsapp");
+//       if (gambar != null){
+////           Picasso.get().load("file://"+gambar).into(imageSender);
+////           imageSender.setVisibility(View.VISIBLE);
+//       }else {
+//           Toast.makeText(getApplicationContext(),"GAMBAR KOSONG",Toast.LENGTH_SHORT).show();
+//       }
+//       btnSend.setOnClickListener(view -> {
+//           message = chat.getText().toString();
+//           sendIamge();
+//       });
     }
+
+    private void backtoChatlist() {
+        ChatFragment fragment = new ChatFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.layoutintenttofragmentchat,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+//    private void sendIamge() {
+//        String aa = "/" + gambar;
+//        RequestBody senderBody = RequestBody.create(MediaType.parse("text/plain"), sender);
+//        RequestBody waBody = RequestBody.create(MediaType.parse("text/plain"), whatsapp);
+//        RequestBody pesanBody = RequestBody.create(MediaType.parse("text/plain"), message);
+//
+//        RequestBody imageBody = RequestBody.create(MediaType.parse("image/*"), gambar);
+//        MultipartBody.Part imagePart = MultipartBody.Part.createFormData("file",gambar, imageBody);
+//        ChatService chatService = ConfigPrivate.htppclient().create(ChatService.class);
+//        Call<SendImageResponse>call = chatService.sendImage(senderBody,waBody,imagePart,pesanBody);
+//        call.enqueue(new Callback<SendImageResponse>() {
+//            @Override
+//            public void onResponse(@NonNull Call<SendImageResponse> call, @NonNull Response<SendImageResponse> response) {
+//                if (response.isSuccessful()){
+//                    SendImageResponse sendImageResponse = response.body();
+//                    if (sendImageResponse.isStatus()){
+//                        Toast.makeText(getApplicationContext(),"sedang mengirim",Toast.LENGTH_SHORT).show();
+//                        Intent intent = new Intent(getApplicationContext(),ChatRoom.class);
+//                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        intent.putExtra("nomor_wa",whatsapp);
+//                        startActivity(intent);
+//                    }else {
+//                        Toast.makeText(getApplicationContext(),"Gagal mengirim gambar",Toast.LENGTH_SHORT).show();
+//                    }
+//                } else {
+//                    Toast.makeText(getApplicationContext(),"server not responding",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<SendImageResponse> call, @NonNull Throwable t) {
+//                Log.d("Response Send Image",t.getMessage());
+//            }
+//        });
+//    }
 
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(this, ChatRoom.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.putExtra("nomor_wa",whatsapp);
-        startActivity(intent);
+        ChatFragment fragment = new ChatFragment();
+        @SuppressLint("CommitTransaction")
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.layoutintenttofragmentchat,fragment).addToBackStack(null).commit();
         super.onBackPressed();
     }
 }
